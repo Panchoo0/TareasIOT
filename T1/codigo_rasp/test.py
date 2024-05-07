71c52186969ca7a7bcc39a07c5a4cc988956dbdc
Francisco Galdames <fgaldames98@gmail.com>
2024-05-07 18:50:47 -0400

    print

--- T1/codigo_rasp/test.py
+++ T1/codigo_rasp/test.py
@@ -2,3 +2,4 @@ from modelos import db, Configuracion
 # Aquí se debe hacer la consulta a la base de dato
 # Configuracion
 ID_protocol2 = Configuracion.get_by_id(1)
+print(ID_protocol2.get_ID_protocol())
