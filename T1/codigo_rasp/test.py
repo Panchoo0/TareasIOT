03561eba44c529b1e14df6f0028cd0e5e97d3afd
Francisco Galdames <fgaldames98@gmail.com>
2024-05-12 10:47:41 -0400

    Update test.py

--- T1/codigo_rasp/test.py
+++ T1/codigo_rasp/test.py
@@ -1,5 +1,7 @@
 from modelos import db, Configuracion
 # Aquí se debe hacer la consulta a la base de dato
 # Configuracion
-ID_protocol2 = Configuracion.get_by_id(1)
-print(ID_protocol2.get_Transport_layer())
+config = Configuracion.get_by_id(1)
+print(config.get_Transport_Layer())
+Configuracion.set_Transport_layer("UDP")
+print(config.get_Transport_Layer())
